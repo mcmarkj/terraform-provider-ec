6d7e463b0e98a80be25127a385a9347f976faa2f
ilya.artamonov <ilya.artamonov@ev-box.com>
2020-08-05 14:54:52 +0200

    refactoring based on PR comments

--- download_binary.sh
+++ download_binary.sh
@@ -2,15 +2,20 @@
 
 ############################################################################################
 #
-# This script downloads terraform provider binary for current OS
+# This script downloads terraform provider ec (elastic search) binary for current OS
+# scripts/download_binary.sh                    - to download binary from release 0.1.0
+# scripts/download_binary.sh {release_version}  - to download binary from provided release version
 #
 ############################################################################################
 
-RELEASE_VERSION=${1:-0.1.0}
+# Will be used if no release version is provided
+DEFAULT_RELEASE_VERSION="0.1.0"
+
+RELEASE_VERSION=${1:-$DEFAULT_RELEASE_VERSION}
 PROVIDER_NAME="terraform-provider-ec"
 TERRAFORM_PLUGIN_DIRECTORY="$HOME/.terraform.d/plugins"
 
-OS=$(uname -a | cut -d " " -f1 | awk '{print tolower($0)}')
+OS=$(uname -s | awk '{print tolower($0)}')
 echo "Operation system: $OS"
 
 BINARY_NAME="${PROVIDER_NAME}_${OS}_amd64"
@@ -18,12 +23,8 @@ BINARY_PATH="https://github.com/everonhq/terraform-provider-ec/releases/download
 
 echo "Downloading binary plugin from $BINARY_PATH to $TERRAFORM_PLUGIN_DIRECTORY ..."
 
-mkdir $TERRAFORM_PLUGIN_DIRECTORY
+mkdir -p $TERRAFORM_PLUGIN_DIRECTORY
 wget -cO - $BINARY_PATH > $TERRAFORM_PLUGIN_DIRECTORY/$PROVIDER_NAME
-if [[ $? != 0 ]]; then
-  echo "Error during provider downloading"
-  exit 1
-fi
 chmod a+x $TERRAFORM_PLUGIN_DIRECTORY/$PROVIDER_NAME
 
 echo "$PROVIDER_NAME downloaded successfully"

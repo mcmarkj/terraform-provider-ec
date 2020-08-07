bd434903f7baa225df60ef1ac6ac4f337db6babe
ilya.artamonov <ilya.artamonov@ev-box.com>
2020-08-07 16:54:02 +0200

    refactoring based on PR comments

--- download_binary.sh
+++ download_binary.sh
@@ -22,9 +22,14 @@ BINARY_NAME="${PROVIDER_NAME}_${OS}_amd64"
 BINARY_PATH="https://github.com/everonhq/terraform-provider-ec/releases/download/$RELEASE_VERSION/$BINARY_NAME"
 
 echo "Downloading binary plugin from $BINARY_PATH to $TERRAFORM_PLUGIN_DIRECTORY ..."
-
 mkdir -p $TERRAFORM_PLUGIN_DIRECTORY
-wget -cO - $BINARY_PATH > $TERRAFORM_PLUGIN_DIRECTORY/$PROVIDER_NAME
+
+if [[ "${OS}" == "darwin" ]]; then
+  curl -LJ $BINARY_PATH --output $TERRAFORM_PLUGIN_DIRECTORY/$PROVIDER_NAME
+else
+  wget -cO - $BINARY_PATH > $TERRAFORM_PLUGIN_DIRECTORY/$PROVIDER_NAME
+fi
+
 chmod a+x $TERRAFORM_PLUGIN_DIRECTORY/$PROVIDER_NAME
 
 echo "$PROVIDER_NAME downloaded successfully"
